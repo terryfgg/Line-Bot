ba017b2e6ae91cc72eaeef88e673d227c737ecac
unknown <terry1997116@gmail.com>
2019-03-09 14:18:51 +0800

    myApp

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ bot.on('message', function(event) {
         function (profile) {            
             return event.reply({
                 "type": "image",
-                "originalContentUrl": "https://terrylin-app.herokuapp.com/imgs/p01.jpg",
-                "previewImageUrl": "https://terry-app.herokuapp.com/imgs/p01.jpg"
+                "originalContentUrl": "https://terrylin-app.herokuapp.com/imgs/01.jpg",
+                "previewImageUrl": "https://terry-app.herokuapp.com/imgs/01.jpg"
             });	            
         }
     );
